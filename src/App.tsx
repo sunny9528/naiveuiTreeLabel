659136013cdeabcd54efafef3ed2cf91538e24a4
sunny1011111 <zhuo75822@163.com>
2022-08-21 15:13:20 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,83 @@
+import { defineComponent, VNodeChild } from 'vue';
+import styled from './app.module.less';
+import { TreeOption, NEllipsis } from 'naive-ui';
+import {NTree} from '../../src/tree'
+
+interface IRenderLabelParams {
+  option: TreeOption;
+  checked: boolean;
+  selected: boolean;
+}
+
+export default defineComponent({
+  setup() {
+    const option = [
+      {
+        key: 1,
+        label: '第一季',
+        children: [
+          {
+            key: '1-1',
+            label: '第一季第一级',
+          },
+          {
+            key: '1-2',
+            label: '第一季第二级第一季第二级第一季第二级第一季第二级第一季第二级',
+          },
+          {
+            key: '1-3',
+            label: '第一季第三级',
+          },
+          {
+            key: '1-4',
+            label: '第一季第四级',
+          },
+        ],
+      },
+      {
+        key: 2,
+        label: '第二季',
+        children: [
+          {
+            key: '2-1',
+            label: '第二季第一级',
+          },
+          {
+            key: '2-2',
+            label: '第二季第二级',
+          },
+          {
+            key: '2-3',
+            label: '第二季第三级',
+          },
+          {
+            key: '2-4',
+            label: '第二季第四级',
+          },
+        ],
+      },
+    ];
+    const renderLabel = (info: IRenderLabelParams): VNodeChild => {
+      return (
+				<NEllipsis
+					style={{width: '100%'}}
+          v-slots={{
+            tooltip: () => info.option.label,
+            default: () => <span style={{ textAlign: 'center' }}>{info.option.label}</span>,
+          }}
+        />
+      );
+    };
+    return {
+      option,
+      renderLabel,
+    };
+  },
+  render() {
+    return (
+      <div class={styled['box']}>
+        <NTree blockLine expandOnClick data={this.option} renderLabel={this.renderLabel}></NTree>
+      </div>
+    );
+  },
+});
